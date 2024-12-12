c13805cb75e18605b0f0093ee62cca17f0751521
lizot <lizoto@yandex.ru>
2024-12-13 01:30:56 +0300

    1.6. Взаимодействие объектов [#166087 #35589]

--- src/main/java/ru/job4j/oop/Ball.java
+++ src/main/java/ru/job4j/oop/Ball.java
@@ -33,8 +33,8 @@ public class Ball {
     class BallStory {
         public static void main(String[] args) {
             Ball kolobok = new Ball ();
-            Hare kurochka = new Hare ();
-            kurochka.tryEat(kolobok);
+            Hare zayts = new Hare ();
+            zayts.tryEat(kolobok);
             Fox lisichka = new Fox ();
             lisichka.tryEat (kolobok);
             Wolf volk = new Wolf ();
