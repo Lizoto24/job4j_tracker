10a3a137e6bf17e0dcf4a440ec69ba97f3f01e30
lizot <lizoto@yandex.ru>
2025-01-22 19:18:46 +0300

    8. Date. Отображение даты. [#310017 #35595]

---
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -0,0 +1,15 @@
+package ru.job4j.tracker;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class StartUI {
+    public static void main(String[] args) {
+        Item item = new Item("cat");
+        System.out.println(item.getCreated());
+        LocalDateTime currentDateTimeFormat = item.getCreated();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+        System.out.println("Текущие дата и время после форматирования: " + currentDateTimeFormat.format(formatter));
+    }
+    }
+
