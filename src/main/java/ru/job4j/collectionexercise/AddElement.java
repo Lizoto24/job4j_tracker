1a229763c2f9d5b9dc3b4a2eb73e9cea550e50d3
lizot <lizoto@yandex.ru>
2025-05-17 14:32:39 +0300

    2. Размер списка и добавление элементов.

---
+++ src/main/java/ru/job4j/collectionexercise/AddElement.java
@@ -0,0 +1,14 @@
+package ru.job4j.collectionexercise;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AddElement {
+    public static boolean addNewElement(List<String> list, String str) {
+    List<String> check = new ArrayList<>(list);
+    list.add(str);
+    return check.size() != list.size();
+}
+}
+
+
