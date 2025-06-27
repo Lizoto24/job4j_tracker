157381f2d01f7fe6d1a2b94c4cc956b92c512ed8
lizot <lizoto@yandex.ru>
2025-06-27 13:37:32 +0300

    1.5. Создание стрима. Метод Stream.builder()

---
+++ src/main/java/stream/StreamBuilder.java
@@ -0,0 +1,10 @@
+package stream;
+
+import java.util.stream.Stream;
+
+public class StreamBuilder {
+    public static Stream<Object> createStream(Integer[] data) {
+        Stream <Object> stream = Stream.builder().add(data[0]).add(data[data.length-1]).build();
+        return stream;
+    }
+}
