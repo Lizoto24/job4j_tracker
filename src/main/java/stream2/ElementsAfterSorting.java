dcf9350028d2c954c8618b6311b74bc9cd9aa6d4
lizot <lizoto@yandex.ru>
2025-07-09 19:34:24 +0300

    Deleted

--- src/main/java/stream2/ElementsAfterSorting.java
+++ src/main/java/stream2/ElementsAfterSorting.java
@@ -5,7 +5,7 @@ import java.util.stream.Collectors;
 
 public class ElementsAfterSorting {
     public static void main(String[] args) {
-        List<Integer> numbers = List.of(2,4,6,7,12,45);
+        List<Integer> numbers = List.of(44,7,3,22,89,1,68);
         List <Integer> result = numbers.stream()
                 .sorted()
                 .limit(3)
