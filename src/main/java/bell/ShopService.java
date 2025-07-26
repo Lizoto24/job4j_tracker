e26216fb45475a9832b4808cd031b3dc2dd267b7
lizot <lizoto@yandex.ru>
2025-07-26 13:54:38 +0300

    поправка к задаче по интерфейсу 2

--- src/main/java/bell/ShopService.java
+++ src/main/java/bell/ShopService.java
@@ -6,35 +6,35 @@ import java.util.List;
 import java.util.Map;
 
 public class ShopService implements Basket {
-    private final Map <String, Integer> map = new HashMap<>();
+    private final Map <String, Integer> basket = new HashMap<>();
 
     @Override
     public void addProduct(String product, int quantity) {
-        map.putIfAbsent(product, quantity);
+        basket.putIfAbsent(product, quantity);
     }
 
     @Override
     public void removeProduct(String product) {
-        map.remove(product);
+        basket.remove(product);
     }
 
     @Override
     public void updateProductQuantity(String product, int quantity) {
-        map.put(product, quantity);
+        basket.put(product, quantity);
     }
 
     @Override
     public void clear() {
-        map.clear();
+        basket.clear();
     }
 
     @Override
     public List<String> getProducts() {
-        return new ArrayList<>(map.keySet());
+        return new ArrayList<>(basket.keySet());
     }
 
     @Override
     public int getProductQuantity(String product) {
-        return map.get(product);
+        return basket.get(product);
     }
 }
